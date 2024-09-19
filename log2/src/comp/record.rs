590c102995b151394d2df7ab252d69011f1ee7dd
Daniel <0243179@up.edu.mx>
2024-09-19 02:14:04 -0600

    update proto

--- log2/src/comp/record.rs
+++ log2/src/comp/record.rs
@@ -1,5 +1,29 @@
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ProduceRequest {
+    #[prost(message, optional, tag = "1")]
+    pub record: ::core::option::Option<Record>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ProduceResponse {
+    #[prost(uint64, tag = "1")]
+    pub offset: u64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ConsumeRequest {
+    #[prost(uint64, tag = "1")]
+    pub offset: u64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ConsumeResponse {
+    #[prost(message, optional, tag = "2")]
+    pub record: ::core::option::Option<Record>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Record {
     #[prost(bytes = "vec", tag = "1")]
     pub value: ::prost::alloc::vec::Vec<u8>,
--- log2/src/main.rs
+++ log2/src/main.rs
@@ -1,7 +1,7 @@
 mod comp {
     pub mod config;
     pub mod index;
-    // pub mod log;
+    pub mod log;
     pub mod record;
     pub mod segments;
     pub mod store;
